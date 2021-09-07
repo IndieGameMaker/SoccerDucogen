d9da8bb86c0ee0be268a2d3c784fd960dfed6d89
Lee Jae Hyun <myleje@gmail.com>
2021-09-07 14:19:55 +0900

    .

--- Assets/02.Scripts/PlayerAgent.cs
+++ Assets/02.Scripts/PlayerAgent.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
+using Unity.MLAgents.Policies; // BehaviourParameters
 
 public class PlayerAgent : Agent
 {
@@ -12,4 +13,13 @@ public class PlayerAgent : Agent
 
     public Team team = Team.Blue;
     public Material[] materials;
+
+    private BehaviorParameters bps;
+    private Transform tr;
+    private Rigidbody rb;
+
+    public override void Initialize()
+    {
+
+    }
 }
